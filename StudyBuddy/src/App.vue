cd9b8238e35b2bd11dff60a4a5392898bd8594cc
thisissteff <steffilimenqi@outlook.com>
2023-10-11 22:25:54 +0800

    initialising vite App

---
+++ StudyBuddy/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        
+
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
