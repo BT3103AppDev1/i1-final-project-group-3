869399fa2523f1d065fbaa5ec79fd66c0021aac6
thisissteff <steffilimenqi@outlook.com>
2023-10-20 00:23:54 +0800

    update format of edit profile page and new component

--- src/views/EditProfileView.vue
+++ src/views/EditProfileView.vue
@@ -2,90 +2,66 @@
     <NavBar/>
     <div style="text-align: center;">
 
-        <div class="buttons" v-show="activeTab === 'account-content'">
-            <button class="cancel"> Cancel </button>
-            <button class="save-profile"> Save Profile</button>
-        </div>
+        <titleandImage/>
 
-        <div class="image-and-title">
-            <div class="image-update">
-                <img :src="uploadedImage || '../assets/friends.png'" class="profile-image">
-                <button class="camera-upload" @click="openUploadDialog">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera-bolt" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M13 20h-8a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v3.5" />
-                        <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                        <path d="M19 16l-2 3h4l-2 3" />
-                    </svg>
-
-                </button>
-             
-
-            </div>
-
-            
-            
-            <h1> Edit Profile </h1>
-        </div>
-
-        <div class="popup" v-show="showUploadDialog">
-            <div class="popup-content">
-                <h2>Upload or Remove Photo</h2>
-                <input type="file" accept="image/*" @change="handleFileUpload" />
-                <button @click="removePhoto">Remove Photo</button>
-                <button @click="closeUploadDialog">Cancel</button>
-            </div>
-        </div>
 
         <div class="content">
             <div class="toggle-tab">
                 <div class="tabs">
-                    <button id="account-tab" class= "tablinks" v-on:click="openSection(event, 'account-content')"
-                            :class="{ active: activeTab === 'account-content' }"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-invoice" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                            <path d="M9 7l1 0"></path>
-                            <path d="M9 13l6 0"></path>
-                            <path d="M13 17l2 0"></path>
-                        </svg>
-                        Account Details
+                    <button id="account-tab" class="tablinks" v-on:click="openSection(event, 'account-content')" :class="{ active: activeTab === 'account-content' }">
+                        <div class="button-content">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-invoice" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                                <path d="M9 7l1 0"></path>
+                                <path d="M9 13l6 0"></path>
+                                <path d="M13 17l2 0"></path>
+                            </svg>
+                            <text>Account Details</text>
+                        </div>
                     </button>
+
                     <span class="tab-space"></span>
 
                     <button id="password-tab" class= "tablinks" v-on:click="openSection(event, 'password-content')"
                             :class="{ active: activeTab === 'password-content' }">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-password-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M12 17v4"></path>
-                            <path d="M10 20l4 -2"></path>
-                            <path d="M10 18l4 2"></path>
-                            <path d="M5 17v4"></path>
-                            <path d="M3 20l4 -2"></path>
-                            <path d="M3 18l4 2"></path>
-                            <path d="M19 17v4"></path>
-                            <path d="M17 20l4 -2"></path>
-                            <path d="M17 18l4 2"></path>
-                            <path d="M9 6a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                            <path d="M7 14a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2"></path>
-                        </svg>
-                        Change Password
+                        <div class="button-content">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-password-user" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M12 17v4"></path>
+                                    <path d="M10 20l4 -2"></path>
+                                    <path d="M10 18l4 2"></path>
+                                    <path d="M5 17v4"></path>
+                                    <path d="M3 20l4 -2"></path>
+                                    <path d="M3 18l4 2"></path>
+                                    <path d="M19 17v4"></path>
+                                    <path d="M17 20l4 -2"></path>
+                                    <path d="M17 18l4 2"></path>
+                                    <path d="M9 6a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                                    <path d="M7 14a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2"></path>
+                            </svg>
+                            <text>Change Password</text>
+                        </div>
+                        
                     </button>
                     <span class="tab-space"></span>
 
                     <button id="blocked-tab" class= "tablinks" v-on:click="openSection(event, 'blocked-content')"
                             :class="{ active: activeTab === 'blocked-content' }">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-                            <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1"></path>
-                            <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-                            <path d="M17 10h2a2 2 0 0 1 2 2v1"></path>
-                            <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-                            <path d="M3 13v-1a2 2 0 0 1 2 -2h2"></path>
-                        </svg>
-                        Blocked Users
+                        <div class="button-content"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" >
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1"></path>
+                                <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                <path d="M17 10h2a2 2 0 0 1 2 2v1"></path>
+                                <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                <path d="M3 13v-1a2 2 0 0 1 2 -2h2"></path>
+                            </svg>
+                            <text>Blocked Users</text> 
+                        </div>   
+                        
                     </button>
 
                 </div>
@@ -94,6 +70,11 @@
 
             <div class="tab-content">
                 <div id="account-content" class="tabcontent" v-show="activeTab === 'account-content'">
+
+                    <div class="buttons" v-show="activeTab === 'account-content'">
+                        <button class="cancel"> Cancel </button>
+                        <button class="save-profile"> Save Profile</button>
+                    </div>
 
                     <div id="first-name" class="input">
                         <h3>First Name</h3>
@@ -194,19 +175,18 @@
 
 <script>
 import NavBar from '../components/NavigationBar.vue';
-
+import titleandImage from '../components/titleandImage.vue';
 
 export default {
     name: "EditProfile",
     components: {
-        NavBar
+        NavBar,
+        titleandImage,
     }, 
 
     data() {
         return {
             activeTab: 'account-content',
-            showUploadDialog: false,
-            uploadedImage: null,
         }
     },
 
@@ -219,32 +199,6 @@ export default {
         created() {
             // Open the default tab when the component is created
             this.openSection('account-content');
-        },
-
-        openUploadDialog() {
-            this.showUploadDialog = true; // Show the dialog when the button is clicked
-        },
-
-        closeUploadDialog() {
-            this.showUploadDialog = false; // Close the dialog when Cancel is clicked
-        },
-
-        removePhoto() {
-            this.uploadedImage = null; // Remove the image
-            this.closeUploadDialog(); // Close the dialog
-        },
-
-        handleFileUpload(event) {
-            // Handle the selected file and update the uploadedImage
-            const file = event.target.files[0];
-            if (file && file.type.startsWith('image/')) {
-                // Check if the file is an image
-                const reader = new FileReader();
-                reader.onload = (e) => {
-                    this.uploadedImage = e.target.result;
-                };
-                reader.readAsDataURL(file);
-            }
         },
 
 
@@ -280,6 +234,12 @@ h3 {
     
 }
 
+.buttons {
+    position: absolute;
+    top: 140px;
+    right: -30px;
+}
+
 .buttons button, 
 .password-buttons button {
     padding: 10px 20px; 
@@ -301,7 +261,7 @@ h3 {
 
 
 .cancel {
-    background-color: white; /* Red color for cancel */
+    background-color: white; 
     color: #525fe1; /* Text color */
     margin-right: 10px;
     border: 2px solid #525fe1;
@@ -360,16 +320,16 @@ h3 {
 }
 
 .content {
-    margin-top: 0px;
+    margin-top: -100px;
     
     
 }
 
 
 
-.active {
-    border-bottom: 2px solid #007BFF; /* Add an underline for the active tab */
-    color: #007BFF;
+.tabs {
+    margin-left: 170px;
+    
 }
 
 .tabs button {
@@ -378,8 +338,13 @@ h3 {
     font-family: inter;
     font-weight: bold;
     font-size: 19px;
+    padding-right: 40px;
+    
+   
+    
     
 }
+
 
 
 .tab-content {
@@ -388,6 +353,38 @@ h3 {
     margin-top: 30px;
     
 }
+
+
+.tablinks.active text, 
+.tablinks.active svg{
+    stroke: #525fe1;
+    font-weight: bold; /* Set the font weight for the active tab */
+    color: #525fe1; /* Set the active tab text color */
+    
+}
+
+
+
+
+.button-content {
+    display: flex;
+    align-items: center;
+    color: #b6b6b6;
+    padding-bottom: 10px;
+}
+
+.button-content text {
+    padding-left:10px;
+    font-weight:lighter;
+}
+
+.button-content:hover {
+    color: #525fe1;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+    border-bottom: 2px solid #525fe1;
+}
+
 
 .input {
     padding-bottom: 30px;
@@ -449,6 +446,13 @@ input[id="description"] {
 .password-buttons button {
     margin-left: 10px; /* Add space between buttons */
 }
+
+
+#account-tab {
+    position: relative;
+    top: 2px;
+}
+
 
 
 
