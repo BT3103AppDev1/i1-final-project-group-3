9987715373e586ba7632f9b988051829ee60cac0
thisissteff <steffilimenqi@outlook.com>
2023-11-08 00:39:35 +0800

    chatting function works but doesnt realtime update

--- src/views/TempChats.vue
+++ src/views/TempChats.vue
@@ -2,15 +2,22 @@
     <div>
         <NavigationBar/>
       <!-- Iterate through the user's field which is an array -->
-      <div>
-        <h1 v-for="(item, index) in userFieldArray" :key="index">{{ item }}</h1>
-      </div>
+        <div v-for="chat in chats" :key="chat.id">
+            <h3>Chat ID: {{ chat.id }}</h3>
+            <ul>
+                <h6 v-for="message in chat.messages" :key="message.id">
+                <strong>{{ message.senderName }}:</strong> 
+                <h1>{{ message.text }} </h1>
+                <div>Time: {{ message.timestamp }}</div>
+                </h6>
+            </ul>
+        </div>
     </div>
   </template>
   
 <script>
 import { ref, onMounted } from 'vue';
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore, doc, getDocs, getDoc, collection, query, orderBy } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebaseApp from '@/firebase.js';
 import NavigationBar from '../components/NavigationBar.vue';
@@ -25,44 +32,75 @@ export default {
     },
         
     setup() {
-    const userFieldArray = ref([]);
+    const chats = ref([]);
     const user = ref(null);
     
     onMounted(async () => {
-        onAuthStateChanged(auth, async (firebaseUser) => {
-            if (firebaseUser) {
-                user.value = firebaseUser;
-                console.log(user.value)
-                await fetchUserField(firebaseUser.uid); // Fetch the fields using UID
+      onAuthStateChanged(auth, async (firebaseUser) => {
+        if (firebaseUser) {
+          await fetchChats(firebaseUser.uid); // Fetch the chats using UID
+        }
+      });
+    });
+
+    const fetchChats = async (uid) => {
+        const userDocRef = doc(db, 'Users', uid);
+        try {
+            const userDocSnap = await getDoc(userDocRef);
+            if (userDocSnap.exists()) {
+            const userChatIds = userDocSnap.data().chatIds; // Assuming 'chatIds' is the field name
+            for (const chatId of userChatIds) {
+                // Note the path 'Message/{chatId}/msglist' should match your collection structure
+                const messagesRef = collection(db, 'Message', chatId, 'msglist');
+                const q = query(messagesRef, orderBy('timestamp', 'desc'));
+                const messagesSnap = await getDocs(q);
                 
+                const messages = [];
+                messagesSnap.forEach((doc) => {
+                // Add each message to the messages array
+                messages.push({
+                    id: doc.id,
+                    text: doc.data().message,
+                    receiverName: doc.data().receiverName,
+                    receiverUID: doc.data().receiverUID,
+                    senderName: doc.data().senderName,
+                    senderUID: doc.data().senderUID,
+                    timestamp: doc.data().timestamp,
+                    // Add other fields as needed
+                });
+                });
+
+                // Add the chat with its messages to the chats array
+                chats.value.push({
+                id: chatId,
+                messages: messages,
+                });
+            }
             } else {
-                user.value = null;
+            console.log('User document does not exist!');
             }
-            });
-        });
-    
-        const fetchUserField = async (uid) => {
-            const userDocRef = doc(db, 'Users', uid);
-            try {
-            const userDoc = await getDoc(userDocRef);
-            if (userDoc.exists()) {
-                // Replace 'fieldName' with the actual field you want to access
-                userFieldArray.value = userDoc.data().chatIds;
-                console.log(userFieldArray.value)
-            } else {
-                console.log('User document does not exist!');
-            }
-            } catch (error) {
-            console.error('Error fetching user document:', error);
-            }
-        };
-    
-        // Return the reactive properties to be used in the template
-        return {
-            userFieldArray,
-            user
-        };
+        } catch (error) {
+            console.error('Error fetching chats:', error);
         }
     };
+
+    
+
+
+    return {
+      chats,
+    };
+    
+        // Return the reactive properties to be used in the template
+        
+    }
+};
 </script>
+
+<style scoped>
+h6{
+    color: black;
+}
+
+</style>
   
