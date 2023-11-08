274baa153820c79d26e35e6bec40e2ee0ec024cc
thisissteff <steffilimenqi@outlook.com>
2023-11-08 15:00:57 +0800

    unstyled chat page with functional chat view and convo list

--- src/views/TempChats.vue
+++ src/views/TempChats.vue
@@ -1,18 +1,29 @@
 <template>
     <div>
         <NavigationBar/>
-      <!-- Iterate through the user's field which is an array -->
-            <div v-for="chat in chats" :key="chat.id">
-            <h3>Chat ID: {{ chat.id }}</h3>
-            <ul>
-                <strong>{{ chat.name }}:</strong>
-                <li>
-                    <!--- getting the person's name--->
-                    <h1>{{ chat.lastMessage.text }}</h1>
-                    <div>Time: {{ chat.lastMessage.timestamp.toDate() }}</div>
-                </li>
-            </ul>
+
+        <!-- Iterate through the user's field which is an array -->
+        <div class="convo-list">
+            <div v-for="chat in chats" :key="chat.id"  @click="selectChat(chat)">
+                <h6>{{ chat.name }}:</h6>                    
+                <!--- getting the person's name--->
+                <h6>{{ chat.lastMessage.text }}</h6>
+                <h6>Time: {{ chat.lastMessage.timestamp.toDate() }}</h6>
+                    
+            </div>
+
         </div>
+
+        <div class="convo-full">
+            <div v-if="selectedChat">
+                <h2>{{ selectedChat.name }}</h2>
+                <div v-for="message in selectedChat.messages" :key="message.id">
+                    <h6>{{ message.senderName }}: {{ message.text }}</h6>
+                    <h6>Time: {{ message.timestamp.toDate() }}</h6>
+                </div>
+            </div>
+        </div>
+            
 
     </div>
   </template>
@@ -36,11 +47,13 @@ export default {
     setup() {
     const chats = ref([]);
     const user = ref(null);
+    const selectedChat = ref(null);
     
     onMounted(async () => {
       onAuthStateChanged(auth, async (firebaseUser) => {
         if (firebaseUser) {
           await fetchChats(firebaseUser.uid); // Fetch the chats using UID
+          
         }
       });
     });
@@ -113,78 +126,53 @@ export default {
         }
     };
 
-    /*
-    const fetchConvo = async (uid) => {
-        const userDocRef = doc(db, 'Users', uid);
-        
+    const selectChat = async (chatSummary) => {
+        const messagesRef = collection(db, 'Message', chatSummary.id, 'msglist');
+
         try {
-            const userDocSnap = await getDoc(userDocRef);
-            if (userDocSnap.exists()) {
-            const userChatIds = userDocSnap.data().chatIds; // Assuming 'chatIds' is the field name
+            const q = query(messagesRef, orderBy('timestamp', 'asc'));
+            const messagesSnap = await getDocs(q);
 
-            for (const chatId of userChatIds) {
-                // Note the path 'Message/{chatId}/msglist' should match your collection structure
-
-                
-                const messagesRef = collection(db, 'Message', chatId, 'msglist');
-                console.log(messagesRef)
-                
-                //getting contact name
-                const chatDocRef = doc(db, 'Message', chatId);
-                const chatDocSnap = await getDoc(chatDocRef);
-                const chatData = chatDocSnap.data();
-                const user1 = chatData.senderUID;
-                const user2 = chatData.receiverUID;
-                console.log(uid)
-                console.log(user1)
-                console.log(user2)
-
-                let name 
-                if (user1 == uid) {
-                    name = chatData.receiverName;
-                } else {
-                    name = chatData.senderName;
-                }
-
-                const q = query(messagesRef, orderBy('timestamp', 'desc'));
-                const messagesSnap = await getDocs(q);
-                
+            // If there are messages, proceed to parse them
+            if (!messagesSnap.empty) {
                 const messages = [];
                 messagesSnap.forEach((doc) => {
-                // Add each message to the messages array
-                messages.push({
-                    id: doc.id,
-                    text: doc.data().message,
-                    receiverName: doc.data().receiverName,
-                    receiverUID: doc.data().receiverUID,
-                    senderName: doc.data().senderName,
-                    senderUID: doc.data().senderUID,
-                    timestamp: doc.data().timestamp,
-                    // Add other fields as needed
+                    messages.push({
+                        id: doc.id,
+                        text: doc.data().message,
+                        receiverName: doc.data().receiverName,
+                        receiverUID: doc.data().receiverUID,
+                        senderName: doc.data().senderName,
+                        senderUID: doc.data().senderUID,
+                        timestamp: doc.data().timestamp,
+                        // Add other fields as needed
+                    });
                 });
-                });
+                console.log(messages)
 
-                // Add the chat with its messages to the chats array
-                chats.value.push({
-                id: chatId,
-                messages: messages,
-                name: name,
-                });
-            }
+                // Assuming chats is a reactive property and you want to update it
+                selectedChat.value = { // 'this' needs to reference the Vue instance
+                    id: chatSummary.id,
+                    messages: messages,
+                    name: chatSummary.name, // Make sure chatSummary has a 'name' property
+                };
             } else {
-            console.log('User document does not exist!');
+                console.log('No messages found!');
             }
         } catch (error) {
             console.error('Error fetching chats:', error);
         }
     };
-    */
+
+    
 
     
 
 
     return {
       chats,
+      selectChat,
+      selectedChat,
     };
     
         // Return the reactive properties to be used in the template
