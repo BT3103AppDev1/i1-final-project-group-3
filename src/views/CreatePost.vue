f3f6b6648e1cbee80fa21fcfb99261fd208a0586
Victoria Kang <victoria.kxh@hotmail.com>
2023-11-12 19:45:09 +0800

    remove image button in create post

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -6,17 +6,6 @@
         <input v-model="header" class="enter-the-header" id="enter-the-header" placeholder="Enter the header here...">
         <div class="description">Description</div>
         <textarea v-model="description" class="enter-the-description" id= "enter-the-description" placeholder="Enter the description here..."></textarea>
-        <button class="insert-image">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-photo-plus" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M15 8h.01"></path>
-            <path d="M12.5 21h-6.5a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6.5"></path>
-            <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l4 4"></path>
-            <path d="M14 14l1 -1c.67 -.644 1.45 -.824 2.182 -.54"></path>
-            <path d="M16 19h6"></path>
-            <path d="M19 16v6"></path>
-          </svg>
-        </button>
         <button class="cancelCreatepost" @click="navigateToPost">Cancel</button>   
         <button type = "submit" id="post-button" @click="submitForm">Post</button>
             
@@ -282,23 +271,6 @@ export default {
     font-size: 20px;
     font-family: var(--font-josefin-sans);
     padding: 20px;
-  }
-  .insert-image {
-    position: absolute;
-    top: 732px;
-    left: 1137px;
-    border-radius: 50%;
-    background-color: #d9d9d9;
-    border: 1px solid #d9d9d9;
-    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
-    width: 55px;
-    height: 55px;
-    cursor: pointer;
-  }
-
-  .insert-image:hover {
-    background-color: #a7a7a7;
-    border: 1px solid #a7a7a7;
   }
 
   .cancelCreatepost {
